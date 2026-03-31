c40d34e3a52d464322397fc0b0acecc569183fbb
danilo-paiva <danilo.paiva.ars@gmail.com>
2026-03-30 21:24:38 -0300

    Add files via upload

---
+++ POOI/1bi/projeto/projeto.py
@@ -0,0 +1,99 @@
+
+print('='*50)
+print(' '*8, end ='')
+print('SISTEMA DE ANÁLISE DE DESEMPENHO')
+print('='*50)
+
+quantidadeAlunos = int(input('Digite a quantidade de alunos: '))
+while quantidadeAlunos <=0:
+    quantidadeAlunos = int(input('Número igual de alunos ou menor que zero \nDigite novamente a quantidade de alunos: '))
+print()
+
+quantidadeDisciplinas = int(input('Digite a quantidade de disciplinas: '))
+while quantidadeDisciplinas <=0:
+    quantidadeDisciplinas = int(input('Número igual de disciplinas ou menor que zero \nDigite novamente a quantidade de disciplinas: '))
+print()
+
+somaMedias = 0
+menorMedia = 10
+maiorMedia = 0
+
+primeiroMedia = 0
+ultimoMedia = 0
+mediasIndividuais = 0.0
+multiplicador = 1
+
+for aluno in range(0,quantidadeAlunos):
+
+    # besterol so pra ficar bonitinho
+    print('-'*50)
+    print(' '*17, end ='')
+    print(f'Notas Aluno {aluno+1}')
+    print('-' * 50)
+
+    somaNotas = 0
+    for disciplina in range(0,quantidadeDisciplinas):
+
+        nota = int(input(f'Digite a nota final da disciplina {disciplina+1}: '))
+
+        while nota<0 or nota>10:
+            nota = int(input(f'Nota não pode ser negativa ou maior que 10 \nDigite novamente a nota final da disciplina {disciplina+1}: '))
+        somaNotas += nota
+
+    mediaIndividual = somaNotas/quantidadeDisciplinas
+    somaMedias += mediaIndividual
+    
+    if mediaIndividual > maiorMedia:
+        maiorMedia = mediaIndividual
+        
+    if mediaIndividual < menorMedia:
+        menorMedia = mediaIndividual
+
+    if aluno == 0:
+        primeiroMedia = mediaIndividual
+    if aluno == quantidadeAlunos-1:
+        ultimoMedia = mediaIndividual
+
+    mediaIndividual = int(mediaIndividual*100)
+    mediasIndividuais += mediaIndividual * multiplicador
+    multiplicador *= 1000
+
+    print()
+
+
+mediaTurma = somaMedias/quantidadeAlunos
+quantidadeAcimaDaMedia = 0
+
+for aluno in range(0,quantidadeAlunos):
+    media = mediasIndividuais%1000
+    mediasIndividuais -= media
+    mediasIndividuais/=1000
+    media/=100
+    if media > mediaTurma:
+        quantidadeAcimaDaMedia+=1
+    #print(media)
+
+
+desempenhoTurma = ""
+if mediaTurma < 5 or menorMedia < 3:
+    desempenhoTurma = "insuficiente"
+elif ultimoMedia > primeiroMedia and mediaTurma>=7:
+    desempenhoTurma = "consistente e positivo"
+elif ultimoMedia < primeiroMedia and mediaTurma<5:
+    desempenhoTurma = "em queda"
+elif ultimoMedia == primeiroMedia:
+    desempenhoTurma = "equilibrado"
+else:
+    desempenhoTurma = "irregular"
+
+print()
+print('-'*50)
+print(' ' * 8, end='')
+print('RESULTADOS E ESTATÍSTICAS DA TURMA')
+print('-'*50)
+
+print(f'Media Turma: {mediaTurma:.1f} \n'
+      f'Alunos acima da média: {quantidadeAcimaDaMedia} \n'
+      f'Maior media individual: {maiorMedia:.2f} \n'
+      f'Menor media individual: {menorMedia:.2f} \n'
+      f'Diagnostico da turma: desempenho {desempenhoTurma}')
