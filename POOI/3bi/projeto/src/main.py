e2cdd117aa7d2b11b2cdb8c971780d1c7538cbd9
danilo-paiva <danilo.paiva.ars@gmail.com>
2026-09-07 16:05:38 -0300

    refactor(pooi-3bi): troca exit por abrebanco com retorno 1-0 padrao apostila

--- POOI/3bi/projeto/src/main.py
+++ POOI/3bi/projeto/src/main.py
@@ -1,28 +1,33 @@
 import mysql.connector
 
-try:
-    conexao = mysql.connector.Connect(
-        host='localhost',
-        database='pooi3bi',
-        user='root',
-        password=''
-    )
-
-    if conexao.is_connected():
-        print('Banco de dados ABERTO: Conexao realizada com banco')
-    else:
-        print('Banco de dados FECHADO: NAO OCORREU Conexao com banco')
-
-except Exception as erro:
-    print(f'Erro : {erro}')
-    exit()
-
 
 VERDE = '\033[92m'    # codigo ANSI: texto verde
 VERMELHO = '\033[91m'  # codigo ANSI: texto vermelho
 PADRAO = '\033[0m'     # codigo ANSI: reseta para a cor padrao do terminal
 
 mensagem = ''   # guarda a mensagem (sucesso ou erro) para mostrar no menu
+
+
+def abrebanco():
+    try:
+        global conexao
+        conexao = mysql.connector.Connect(
+            host='localhost',
+            database='pooi3bi',
+            user='root',
+            password=''
+        )
+
+        if conexao.is_connected():
+            print('Banco de dados ABERTO: Conexao realizada com banco')
+            return 1
+        else:
+            print('Banco de dados FECHADO: NAO OCORREU Conexao com banco')
+            return 0
+
+    except Exception as erro:
+        print(f'Erro : {erro}')
+        return 0
 
 
 def ler_inteiro(texto):
@@ -243,8 +248,9 @@ def consultarAlunosDaTurma():
 
 
 # ====== MENU ======
-while True:
-    print('''
+if abrebanco() == 1:
+    while True:
+        print('''
 ===== SISTEMA TURMAS E ALUNOS =====
 1  - Cadastrar turma
 2  - Alterar turma
@@ -259,37 +265,39 @@ while True:
 0  - Sair
 ''')
 
-    if mensagem != '':
-        print(f'>>> {mensagem}')
-        mensagem = ''      # limpa a mensagem depois de mostrar
+        if mensagem != '':
+            print(f'>>> {mensagem}')
+            mensagem = ''      # limpa a mensagem depois de mostrar
 
-    opcao = ler_inteiro('Escolha uma opcao:\n> ')
+        opcao = ler_inteiro('Escolha uma opcao:\n> ')
 
-    if opcao == 1:
-        cadastrarTurma()
-    elif opcao == 2:
-        alterarTurma()
-    elif opcao == 3:
-        excluirTurma()
-    elif opcao == 4:
-        consultarTurmas()
-    elif opcao == 5:
-        cadastrarAluno()
-    elif opcao == 6:
-        alterarAluno()
-    elif opcao == 7:
-        excluirAluno()
-    elif opcao == 8:
-        consultarAlunos()
-    elif opcao == 9:
-        consultarTurmaDoAluno()
-    elif opcao == 10:
-        consultarAlunosDaTurma()
-    elif opcao == 0:
-        print('Encerrando o sistema...')
-        break
-    else:
-        mensagem = f'{VERMELHO}Opcao invalida!{PADRAO}'
+        if opcao == 1:
+            cadastrarTurma()
+        elif opcao == 2:
+            alterarTurma()
+        elif opcao == 3:
+            excluirTurma()
+        elif opcao == 4:
+            consultarTurmas()
+        elif opcao == 5:
+            cadastrarAluno()
+        elif opcao == 6:
+            alterarAluno()
+        elif opcao == 7:
+            excluirAluno()
+        elif opcao == 8:
+            consultarAlunos()
+        elif opcao == 9:
+            consultarTurmaDoAluno()
+        elif opcao == 10:
+            consultarAlunosDaTurma()
+        elif opcao == 0:
+            print('Encerrando o sistema...')
+            break
+        else:
+            mensagem = f'{VERMELHO}Opcao invalida!{PADRAO}'
 
-conexao.close()
-print('Banco de dados FECHADO.')
+    conexao.close()
+    print('Banco de dados FECHADO.')
+else:
+    print('FIM DO PROGRAMA! Algum problema na conexao com banco de dados.')
